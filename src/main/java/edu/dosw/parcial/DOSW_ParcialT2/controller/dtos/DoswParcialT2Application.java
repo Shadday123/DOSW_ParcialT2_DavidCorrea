8f87ab64ca8dc5c10d2ad2a7d351eb6b10d35d41
unknown <david.correa-g@labinfo.is.escuelaing.edu.co>
2026-04-07 09:45:59 -0500

    Configuración base de datos

---
+++ src/main/java/edu/dosw/parcial/DOSW_ParcialT2/controller/dtos/DoswParcialT2Application.java
@@ -0,0 +1,13 @@
+package edu.dosw.parcial.DOSW_ParcialT2.controller.dtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoswParcialT2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoswParcialT2Application.class, args);
+	}
+
+}
